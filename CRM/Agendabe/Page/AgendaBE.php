8a3f50ab265bee7bccff0afbc0e90c6903459e8a
xavier <xavier@tttp.eu>
2014-12-02 13:37:05 +0100

    add xml header

--- CRM/Agendabe/Page/AgendaBE.php
+++ CRM/Agendabe/Page/AgendaBE.php
@@ -4,6 +4,8 @@ require_once 'CRM/Core/Page.php';
 
 class CRM_Agendabe_Page_AgendaBE extends CRM_Core_Page {
   function run() {
+    header("Content-Type: application/xhtml+xml; charset=utf-8");
     echo civicrm_api3("Event","generateagendabe",array()); 
+    CRM_Utils_System::civiExit();
   }
 }
