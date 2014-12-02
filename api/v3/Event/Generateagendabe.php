9d99eb3b0aeeb0cab22599c023d2b82d5c06e887
xavier <xavier@tttp.eu>
2014-12-02 12:29:21 +0100

    generate xml as api

---
+++ api/v3/Event/Generateagendabe.php
@@ -0,0 +1,47 @@
+<?php
+
+function civicrm_api3_event_generateagendabe ($params) {
+  $fields="title,start_date,intro_text,description,end_date,event_type_id,is_monetary,loc_block_id";
+  //$fields="title,start_date,loc_block_id";
+
+  $r = civicrm_api3 ("event","get", array (
+    "start_date" => array (">=" => "now"),
+    "return" => $fields,
+    "options" => array ("limit" => 100),
+  ));
+
+  $locs = civicrm_api3("LocBlock","get",array("options"=>array("limit",1000)));
+  $locs = $locs["values"];
+
+  $xml = new SimpleXMLElement('<events/>');
+  foreach ($r["values"] as $event) {
+    $xe = $xml->addChild("event");
+    $xe->addChild("id",$event["id"]);
+    $d=$xe->addChild("detail");
+    $d->addChild("title",$event["title"]); 
+    $d->addChild("shortDescription",CRM_Utils_String::htmlToText($event["intro_text"])); 
+    $d->addChild("longDescription",CRM_Utils_String::htmlToText($event["description"])); 
+
+    $d = $xe->addChild("dates");
+    $d1 = $d->addChild("date");
+    $d1->addChild("day",substr($event["start_date"],0,10));
+    $d1->addChild("hourstart",substr($event["start_date"],11));
+    $d1->addChild("hourend",substr($event["end_date"],11));
+  //  foreach ($event as $k => $v) {    $xe->addChild($k,$v);  }
+    if ($event["loc_block_id"] && array_key_exists ($event["loc_block_id"],$locs)) {
+      $l = $locs[$event["loc_block_id"]];
+      if (array_key_exists("address_id",$l) && !array_key_exists("address",$l)) {
+        $r = civicrm_api3 ("address","getsingle", array ("id"=> $l["address_id"]));
+        $locs[$event["loc_block_id"]]["address"] = $r;
+      }
+      $p = $xe->addChild("place");
+      $p->addChild("id",$locs[$event["loc_block_id"]]["address"]["id"]);
+      $p->addChild("name",$locs[$event["loc_block_id"]]["address"]["name"]);
+      $p->addChild("street",$locs[$event["loc_block_id"]]["address"]["street_address"]);
+      $p->addChild("zip",$locs[$event["loc_block_id"]]["address"]["postal_code"]);
+      $p->addChild("city",$locs[$event["loc_block_id"]]["address"]["city"]);
+
+    }
+  } 
+  print $xml->asXML();
+}
---
+++ eu.php
@@ -0,0 +1,108 @@
+<?php
+
+require_once 'eu.civix.php';
+
+/**
+ * Implementation of hook_civicrm_config
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
+ */
+function eu_civicrm_config(&$config) {
+  _eu_civix_civicrm_config($config);
+}
+
+/**
+ * Implementation of hook_civicrm_xmlMenu
+ *
+ * @param $files array(string)
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
+ */
+function eu_civicrm_xmlMenu(&$files) {
+  _eu_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implementation of hook_civicrm_install
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
+ */
+function eu_civicrm_install() {
+  _eu_civix_civicrm_install();
+}
+
+/**
+ * Implementation of hook_civicrm_uninstall
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
+ */
+function eu_civicrm_uninstall() {
+  _eu_civix_civicrm_uninstall();
+}
+
+/**
+ * Implementation of hook_civicrm_enable
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
+ */
+function eu_civicrm_enable() {
+  _eu_civix_civicrm_enable();
+}
+
+/**
+ * Implementation of hook_civicrm_disable
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
+ */
+function eu_civicrm_disable() {
+  _eu_civix_civicrm_disable();
+}
+
+/**
+ * Implementation of hook_civicrm_upgrade
+ *
+ * @param $op string, the type of operation being performed; 'check' or 'enqueue'
+ * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
+ *
+ * @return mixed  based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
+ *                for 'enqueue', returns void
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
+ */
+function eu_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _eu_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implementation of hook_civicrm_managed
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
+ */
+function eu_civicrm_managed(&$entities) {
+  _eu_civix_civicrm_managed($entities);
+}
+
+/**
+ * Implementation of hook_civicrm_caseTypes
+ *
+ * Generate a list of case-types
+ *
+ * Note: This hook only runs in CiviCRM 4.4+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
+ */
+function eu_civicrm_caseTypes(&$caseTypes) {
+  _eu_civix_civicrm_caseTypes($caseTypes);
+}
+
+/**
+ * Implementation of hook_civicrm_alterSettingsFolders
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
+ */
+function eu_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
+  _eu_civix_civicrm_alterSettingsFolders($metaDataFolders);
+}
